c73367b0f45a18a005258704dbba2c69a52a7dc3
Samikshashrestha08 <84307125+Samikshashrestha08@users.noreply.github.com>
2022-12-11 22:37:34 +0545

    workshop

---
+++ Week 2/personal/workshop.py
@@ -0,0 +1,4 @@
+#import module in python
+import constants
+print('there are '+ str(constants.MENTORS_COUNT) +' number of mentors')
+print('there are '+str(constants.STUDENTS_COUNT)+' number of students')
